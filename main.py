6d6a1b555f3a4b2c186aae0fc9ae9d8df209ef2d
Bas Pennings <baspster@gmail.com>
2014-02-04 19:56:46 +0100

    Command handler and history for textfield

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import libtcodpy as libtcod
 import textfield
+import core
 
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
+SCREEN_WIDTH = 50
+SCREEN_HEIGHT = 30
 
 FONT_FLAGS = libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD
-FONT_FILE = 'fonts/dejavu10x10_gs_tc.png'
+FONT_FILE = 'fonts/dejavu12x12_gs_tc.png'
 
 LIMIT_FPS = 20
 
@@ -14,14 +15,23 @@ TITLE = 'Neko (lost kitty)'
 libtcod.console_set_custom_font(FONT_FILE, FONT_FLAGS)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE, False)
 
-tf = textfield.TextField(1, 1, 40, 1)
+run = True
 
-while not libtcod.console_is_window_closed():
+def command_handler(s):
+    global run
+    words = core.parse_words(s)
+    print words
+    if len(words) > 0:
+        if words[0] == '@quit':
+            run = False
+
+tf = textfield.TextField(1, SCREEN_HEIGHT - 2, SCREEN_WIDTH - 2, 1,
+    handler=command_handler)
+
+while not libtcod.console_is_window_closed() and run:
     libtcod.console_set_default_foreground(0, libtcod.white)
     libtcod.console_clear(0)
     tf.render(0)
     libtcod.console_flush()
     key = libtcod.console_check_for_keypress(libtcod.KEY_PRESSED)
     tf.update(key)
-    if key.vk == libtcod.KEY_ENTER:
-        tf.reset()
--- textfield.py
+++ textfield.py
@@ -2,7 +2,7 @@ import array
 import libtcodpy as libtcod
 
 class TextField:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, handler=lambda x: None, prompt='', max_history=100):
         self.x = x
         self.y = y
         self.w = w
@@ -11,22 +11,23 @@ class TextField:
         self.fg = libtcod.Color(255, 255, 255)
         self.bg = libtcod.Color(0, 0, 0)
         self.transparency = 1.0
-        self.prompt = ''
+        self.prompt = prompt
         self.text = array.array('c')
-        self.text_x = 0
+        self.text_x = len(prompt)
         self.text_y = 0
         self.cursor_pos = 0
-
-    def prompt(self, v=None):
-        if v != None:
-            self.prompt = v
-            self.text_x = len(self.prompt)
-        return self.prompt
+        self.interval = 1600
+        self.half_interval = self.interval / 2
+        self.on_command = handler
+        self.max_history = max_history
+        self.history = []
+        self.history_i = -1
 
     def _insert_char(self, c):
         """Inserts a character at cursor position."""
-        self.text.insert(self.cursor_pos, c)
-        self.cursor_pos += 1
+        if self.cursor_pos < self.w - 1:
+            self.text.insert(self.cursor_pos, c)
+            self.cursor_pos += 1
 
     def _delete_char(self):
         """Deletes a character at cursor position."""
@@ -35,10 +36,33 @@ class TextField:
             self.cursor_pos -= 1
 
     def _get_cursor_coords(self):
-        """Convers current cursor_pos into console coordinates."""
+        """Converts current cursor_pos into console coordinates."""
         x = self.text_x + self.cursor_pos
         y = 0
         return x, y
+
+    def _update_history(self, text):
+        """Updates internal command history."""
+        if len(self.history) > 0:
+            if text != self.history[0]:
+                self.history.insert(0, text)
+        else:
+            self.history.insert(0, text)
+        # Exceeding the max history length, remove the oldest item
+        if len(self.history) > self.max_history:
+            self.history.pop()       
+
+    def get_text(self):
+        return self.text.tostring()
+
+    def set_text(self, v):
+        self.text = array.array('c', v)
+        self.cursor_pos = len(self.text)
+
+    def reset(self):
+        self.history_i = -1
+        self.cursor_pos = 0
+        self.text = array.array('c')
 
     def update(self, key):
         if key.vk == libtcod.KEY_BACKSPACE:
@@ -53,29 +77,38 @@ class TextField:
         elif key.vk == libtcod.KEY_RIGHT:
             if self.cursor_pos < len(self.text):
                 self.cursor_pos += 1
+        elif key.vk == libtcod.KEY_UP:
+            if self.history_i < len(self.history) - 1:
+                self.history_i += 1
+                self.set_text(self.history[self.history_i])
+        elif key.vk == libtcod.KEY_DOWN:
+            if self.history_i > 0:
+                self.history_i -= 1
+                self.set_text(self.history[self.history_i])
+        elif key.vk == libtcod.KEY_HOME:
+            self.cursor_pos = 0
+        elif key.vk == libtcod.KEY_END:
+            self.cursor_pos = len(self.text)
+        elif key.vk == libtcod.KEY_ENTER:
+            t = self.get_text()
+            self._update_history(t)
+            self.on_command(t)
+            self.reset()
         elif key.c > 31:
             self._insert_char(chr(key.c))
 
-    def reset(self):
-        self.cursor_pos = 0
-        self.text = array.array('c')
-
     def render(self, con):
         time = libtcod.sys_elapsed_milli()
-
         libtcod.console_set_default_foreground(self.con, self.fg)
         libtcod.console_set_default_background(self.con, self.bg)
-        libtcod.console_clear(self.con)       
-        
+        libtcod.console_clear(self.con)         
         flags = libtcod.BKGND_SET | libtcod.LEFT 
         libtcod.console_print_rect_ex(self.con, 0, 0, self.w, self.h, flags, '%s', self.prompt)
-        
         cur_x = self.text_x
         for c in self.text:
             libtcod.console_set_char(self.con, cur_x, 0, c)
-            cur_x += 1
-        
-        cursor_on = (time % 800) > 400       
+            cur_x += 1      
+        cursor_on = (time % self.interval) > self.half_interval       
         cursor_x, cursor_y = self._get_cursor_coords()
         if cursor_on:
             libtcod.console_set_char_foreground(self.con, cursor_x, cursor_y, self.bg)
@@ -83,5 +116,4 @@ class TextField:
         else:
             libtcod.console_set_char_foreground(self.con, cursor_x, cursor_y, self.fg)
             libtcod.console_set_char_background(self.con, cursor_x, cursor_y, self.bg, libtcod.BKGND_SET)
-        
         libtcod.console_blit(self.con, 0, 0, self.w, self.h, con, self.x, self.y, 1.0, self.transparency)
