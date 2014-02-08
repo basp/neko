867fa4894e3312f50cccc91ddf118bf0be343058
Bas Pennings <baspster@gmail.com>
2014-02-08 20:28:03 +0100

    Object and preposition parsing

--- command.py
+++ command.py
@@ -14,18 +14,60 @@ import unittest
 
 def parse(tokens):
     args = tokens[1:]
+    dobjstr, prepstr, iobjstr = parse_args(args)
     return {
         'verb'      : tokens[0], 
         'args'      : args, 
-        'argstr'    : str.join(' ', args)
+        'argstr'    : str.join(' ', args),
+        'dobjstr'   : dobjstr,
+        'prepstr'   : prepstr,
+        'iobjstr'   : iobjstr
     }
 
+preps = {
+    'with',
+    'using',
+    'at'
+}
+
+def parse_args(tokens):
+    count = len(tokens)
+    for i in range(count):
+        maybe_prep0 = tokens[i]
+        if i < count - 1:
+            maybe_prep1 = str.join(' ', [maybe_prep0, tokens[i + 1]])
+        else:
+            maybe_prep1 = None
+        if i < count - 2:
+            maybe_prep2 = str.join(' ', [maybe_prep1, tokens[i + 2]])
+        else:
+            maybe_prep2 = None
+        if maybe_prep2 in preps:
+            return str.join(' ', tokens[0:i]), maybe_prep2, str.join(' ', tokens[i + 3:])
+        elif maybe_prep1 in preps:
+            return str.join(' ', tokens[0:i]), maybe_prep1, str.join(' ', tokens[i + 2:])
+        elif maybe_prep0 in preps:
+            return str.join(' ', tokens[0:i]), maybe_prep0, str.join(' ', tokens[i + 1:])
+    return str.join(' ', tokens), '', ''
+            
 class Parsing(unittest.TestCase):
     def test_parse_verb(self):
-        cmd = parse(['foo', 'bar', 'quux'])
+        cmd = parse(['foo'])
         self.assertEqual('foo', cmd['verb'])
+
+    def test_parse_args(self):
+        cmd = parse(['foo', 'bar', 'quux'])
         self.assertEqual('bar quux', cmd['argstr'])
         self.assertEqual(['bar', 'quux'], cmd['args'])
+
+    def test_parse_dobj(self):
+        cmd = parse(['foo', 'bar', 'quux'])
+        self.assertEqual('bar quux', cmd['dobjstr'])
+
+    def test_parse_iobj(self):
+        cmd = parse(['foo', 'bar', 'quux', 'with', 'zoz', 'nix'])
+        self.assertEqual('bar quux', cmd['dobjstr'])
+        self.assertEqual('zoz nix', cmd['iobjstr'])
 
 if __name__ == '__main__':
     unittest.main()
